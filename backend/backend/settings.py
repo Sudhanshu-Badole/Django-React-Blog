07e686d656beaab84bdb557819743f1f7bc8e68c
Sudhanshu-Badole <sudhanshubadole@gmail.com>
2024-08-19 20:24:03 +0530

    feat:Post Model Structure

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -189,13 +189,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # Custom Admin Settings
 JAZZMIN_SETTINGS = {
-    "site_title": "Desphixs",
-    "site_header": "Desphixs",
+    "site_title": "BlackPearl",
+    "site_header": "BlackPearl",
     "site_brand": "Modern Marketplace ",
     # "site_icon": "images/favicon.ico",
     # "site_logo": "images/logos/logo.jpg",
-    "welcome_sign": "Welcome To Desphixs",
-    "copyright": "Desphixs",
+    "welcome_sign": "Welcome To BlackPearl",
+    "copyright": "BlackPearl",
     "user_avatar": "images/photos/logo.jpg",
     "topmenu_links": [
         {"name": "Dashboard", "url": "home", "permissions": ["auth.view_user"]},
