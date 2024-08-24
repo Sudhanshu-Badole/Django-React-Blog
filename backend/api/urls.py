b46b593e086e942f55cc318b9bf0bb38c8d0ed62
Sudhanshu-Badole <sudhanshubadole@gmail.com>
2024-08-24 10:15:46 +0530

    feat:Cateogry List API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('user/token/', api_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/register/', api_views.RegisterView.as_view(), name='auth_register'),
-    path('user/profile/<user_id>/', api_views.ProfileView.as_view(), name='user_profile')
+    path('user/profile/<user_id>/', api_views.ProfileView.as_view(), name='user_profile'),
 
-
+    # Post Endpoints
+    path('post/category/list/', api_views.CategoryListAPIView.as_view()),
+    path('post/category/posts/<category_slug>/', api_views.PostCategoryListAPIView.as_view()),
 ]
